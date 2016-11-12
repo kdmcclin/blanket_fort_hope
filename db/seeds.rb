264358699386cea08f057c71f8f42dd20ba5f173
Katherine McClintic <kdmcclin@gmail.com>
2016-11-12 12:32:37 -0500

    Start database seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+MedicalNeed.create!([
+  {term: "HIV"}, 
+  {term: "pregnancy"}, 
+  {term: "malnourishment"}, 
+  {term: "diabetes"}, 
+  {term: "miscarriage(s)"}
+])
+
+Survivor.create!([
+  {pseudonym: "Joan", age: 16, gender: "female"},
+  {pseudonym: "Daniel", age: 13, gender: "male", lgbtq: "bisexual"},
+  {pseudonym: "Em", age: 14, gender: "transgender"}
+])
+
+SurvivorMedicalNeed.create!([
+  {survivor_id: 1, medical_need_id: 5},
+  {survivor_id: 1, medical_need_id: 1},
+  {survivor_id: 2, medical_need_id: 3},
+  {survivor_id: 3, medical_need_id: 1}
+])
